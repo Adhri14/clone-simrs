e759e11f56f4e40eee28beb79af087be12ca0fbf
marwandhiaurrahman <marwandhiaurrahman@gmail.com>
2022-11-16 17:11:57 +0700

    localhost storeage

--- app/Http/Controllers/Admin/BarcodeController.php
+++ app/Http/Controllers/Admin/BarcodeController.php
@@ -18,7 +18,7 @@ class BarcodeController extends Controller
         }
         $generator = new BarcodeGeneratorJPG();
         QrCode::backgroundColor(225, 250, 255)->size(250)->format('png')->merge(asset('rsudwaled_icon_qrcode.png'), .15, true)->generate($request->barcode, "public/storage/qrcode_test.png");
-        file_put_contents('storage/barcode_test.png', $generator->getBarcode($request->barcode, $generator::TYPE_CODE_128,  3, 100,));
+        file_put_contents('public/storage/barcode_test.png', $generator->getBarcode($request->barcode, $generator::TYPE_CODE_128,  3, 100,));
         return view('admin.bar_qr_scanner', compact([
             'request',
         ]));
