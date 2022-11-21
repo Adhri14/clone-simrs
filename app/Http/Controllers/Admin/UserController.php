ace8ae732f7cb2af927c43e0ebeac1b1f0ef6284
marwandhiaurrahman <marwandhiaurrahman@gmail.com>
2022-11-21 08:52:02 +0700

    verifikasi user

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -102,6 +102,6 @@ class UserController extends Controller
         $request['number'] = $user->phone;
         $wa->send_message($request);
         Alert::success('Success', 'Akun telah diverifikasi');
-        return redirect()->route('user.index');
+        return back();
     }
 }
