82c9bf9c78ddb33d5b3caf28666eb06d839079e8
marwandhiaurrahman <marwandhiaurrahman@gmail.com>
2022-11-22 18:01:43 +0700

    tambahan

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -179,6 +179,122 @@
 
             </div>
         </section><!-- End About Section -->
+        <section id="departments" class="departments">
+            <div class="container">
+                <div class="section-title">
+                    <h2>Departments</h2>
+                    <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit
+                        sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias
+                        ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
+                </div>
+                <div class="row gy-4">
+                    <div class="col-lg-3">
+                        <ul class="nav nav-tabs flex-column" role="tablist">
+                            <li class="nav-item" role="presentation"> <a class="nav-link show" data-bs-toggle="tab"
+                                    href="#tab-1" aria-selected="false" role="tab"
+                                    tabindex="-1">Senin</a></li>
+                            <li class="nav-item" role="presentation"> <a class="nav-link active"
+                                    data-bs-toggle="tab" href="#tab-2" aria-selected="true"
+                                    role="tab">Selasa</a></li>
+                            <li class="nav-item" role="presentation"> <a class="nav-link" data-bs-toggle="tab"
+                                    href="#tab-3" aria-selected="false" role="tab"
+                                    tabindex="-1">Rabu</a></li>
+                            <li class="nav-item" role="presentation"> <a class="nav-link" data-bs-toggle="tab"
+                                    href="#tab-4" aria-selected="false" role="tab"
+                                    tabindex="-1">Kamis</a></li>
+                            <li class="nav-item" role="presentation"> <a class="nav-link" data-bs-toggle="tab"
+                                    href="#tab-5" aria-selected="false" role="tab" tabindex="-1">Jumat</a>
+                            </li>
+                            <li class="nav-item" role="presentation"> <a class="nav-link" data-bs-toggle="tab"
+                                href="#tab-5" aria-selected="false" role="tab" tabindex="-1">Sabtu</a>
+                        </li>
+                        </ul>
+                    </div>
+                    <div class="col-lg-9">
+                        <div class="tab-content">
+                            <div class="tab-pane" id="tab-1" role="tabpanel">
+                                <div class="row gy-4">
+                                    <div class="col-lg-8 details order-2 order-lg-1">
+                                        <h5><b>Poliklinik Mata</b></h5>
+                                        <p class="fst-italic">Qui laudantium consequatur laborum sit qui ad sapiente
+                                            dila parde sonata raqer a videna mareta paulona marka</p>
+                                        <p>Et nobis maiores eius. Voluptatibus ut enim blanditiis atque harum sint.
+                                            Laborum eos ipsum ipsa odit magni. Incidunt hic ut molestiae aut qui. Est
+                                            repellat minima eveniet eius et quis magni nihil. Consequatur dolorem
+                                            quaerat quos qui similique accusamus nostrum rem vero</p>
+                                    </div>
+                                    <div class="col-lg-4 text-center order-1 order-lg-2"> <img
+                                            src="assets/img/departments-1.jpg" alt="" class="img-fluid">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="tab-pane active show" id="tab-2" role="tabpanel">
+                                <div class="row gy-4">
+                                    <div class="col-lg-8 details order-2 order-lg-1">
+                                        <p><b>Poliklinik Mata</b></p>
+                                        <p>Ea ipsum voluptatem consequatur quis est. Illum error ullam omnis quia et
+                                            reiciendis sunt sunt est. Non aliquid repellendus itaque accusamus eius et
+                                            velit ipsa voluptates. Optio nesciunt eaque beatae accusamus lerode pakto
+                                            madirna desera vafle de nideran pal</p>
+                                    </div>
+                                    <div class="col-lg-4 text-center order-1 order-lg-2"> <img
+                                            src="assets/img/departments-2.jpg" alt="" class="img-fluid">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="tab-pane" id="tab-3" role="tabpanel">
+                                <div class="row gy-4">
+                                    <div class="col-lg-8 details order-2 order-lg-1">
+                                        <h3>Impedit facilis occaecati odio neque aperiam sit</h3>
+                                        <p class="fst-italic">Eos voluptatibus quo. Odio similique illum id quidem non
+                                            enim fuga. Qui natus non sunt dicta dolor et. In asperiores velit quaerat
+                                            perferendis aut</p>
+                                        <p>Iure officiis odit rerum. Harum sequi eum illum corrupti culpa veritatis
+                                            quisquam. Neque necessitatibus illo rerum eum ut. Commodi ipsam minima
+                                            molestiae sed laboriosam a iste odio. Earum odit nesciunt fugiat sit ullam.
+                                            Soluta et harum voluptatem optio quae</p>
+                                    </div>
+                                    <div class="col-lg-4 text-center order-1 order-lg-2"> <img
+                                            src="assets/img/departments-3.jpg" alt="" class="img-fluid">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="tab-pane" id="tab-4" role="tabpanel">
+                                <div class="row gy-4">
+                                    <div class="col-lg-8 details order-2 order-lg-1">
+                                        <h3>Fuga dolores inventore laboriosam ut est accusamus laboriosam dolore</h3>
+                                        <p class="fst-italic">Totam aperiam accusamus. Repellat consequuntur iure
+                                            voluptas iure porro quis delectus</p>
+                                        <p>Eaque consequuntur consequuntur libero expedita in voluptas. Nostrum ipsam
+                                            necessitatibus aliquam fugiat debitis quis velit. Eum ex maxime error in
+                                            consequatur corporis atque. Eligendi asperiores sed qui veritatis aperiam
+                                            quia a laborum inventore</p>
+                                    </div>
+                                    <div class="col-lg-4 text-center order-1 order-lg-2"> <img
+                                            src="assets/img/departments-4.jpg" alt="" class="img-fluid">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="tab-pane" id="tab-5" role="tabpanel">
+                                <div class="row gy-4">
+                                    <div class="col-lg-8 details order-2 order-lg-1">
+                                        <h3>Est eveniet ipsam sindera pad rone matrelat sando reda</h3>
+                                        <p class="fst-italic">Omnis blanditiis saepe eos autem qui sunt debitis porro
+                                            quia.</p>
+                                        <p>Exercitationem nostrum omnis. Ut reiciendis repudiandae minus. Omnis
+                                            recusandae ut non quam ut quod eius qui. Ipsum quia odit vero atque qui
+                                            quibusdam amet. Occaecati sed est sint aut vitae molestiae voluptate vel</p>
+                                    </div>
+                                    <div class="col-lg-4 text-center order-1 order-lg-2"> <img
+                                            src="assets/img/departments-5.jpg" alt="" class="img-fluid">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     </main><!-- End #main -->
     <!-- ======= Footer ======= -->
     <footer id="footer">
