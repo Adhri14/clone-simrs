51c64fd41c156be2c2d7c0a90bacfc7a33d9c578
marwandhiaurrahman <marwandhiaurrahman@gmail.com>
2022-12-06 11:14:03 +0700

    handling error

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,13 @@
+@extends('adminlte::page')
+@section('title', 'Halaman Tidak Ditemukan (404)')
+@section('content_header')
+    <h1>Halaman Tidak Ditemukan (404)</h1>
+@stop
+@section('content')
+    <div class="row">
+        <div class="col-md-12">
+            Silahkan periksa kembali link anda
+        </div>
+    </div>
+
+@stop
