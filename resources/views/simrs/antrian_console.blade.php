807175db35fd77206945fedd295c92f6d76e6a1f
marwandhiaurrahman <marwandhiaurrahman@gmail.com>
2023-03-07 16:33:33 +0700

    perbaikan consil anterian

--- resources/views/simrs/antrian_console.blade.php
+++ resources/views/simrs/antrian_console.blade.php
@@ -149,8 +149,9 @@
         </x-slot>
     </x-adminlte-modal>
 @stop
-@section('plugins.Sweetalert2', true)
-{{-- @section('plugins.Datatables', true) --}}
+@section('plugins.Sweetalert2', true)*
+
+
 @include('sweetalert::alert')
 @section('adminlte_css')
     {{-- <script src="{{ asset('vendor/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css') }}"></script> --}}
